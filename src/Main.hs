43346734ae5b9a4ecd7db81f5051f0a193f35c98
shivaraj-bh <me@shivaraj-bh.in>
2025-04-16 16:49:02 +0530

    final test

--- src/Main.hs
+++ src/Main.hs
@@ -12,4 +12,4 @@ main :: IO ()
 main = do
   -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
   Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎 (from haskell-incremental-build-template)"
+    putTextLn "Hello 🌎 (from haskell-incremental-build-template test)"
