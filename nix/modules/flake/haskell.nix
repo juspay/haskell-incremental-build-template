c960507f7a59da504f677ef7bcb5e2904c895329
shivaraj-bh <me@shivaraj-bh.in>
2025-04-25 11:15:51 +0530

    Encapsulate `previousIntermediates`

--- nix/modules/flake/haskell.nix
+++ nix/modules/flake/haskell.nix
@@ -20,21 +20,17 @@
       settings = {
         haskell-incremental-build-template = {
           installIntermediates = true;
+          # NOTE: `inputs.prev != inputs.self` is a very loose check and expects the user
+          # to do the right thing when overriding `prev`
+          previousIntermediates =
+            if inputs.prev != inputs.self then
+              inputs'.prev.packages.default
+            else null;
           # TODO: Enable `separateIntermediatesOutput` after https://github.com/srid/devour-flake/issues/31 is fixed
         };
       };
-
-      # We don't autoWire the flake outputs to prevent [om ci](https://omnix.page/om/ci.html) from building them. 
-      # Instead, we have `packages.default` (see below) that conditionally does incremental or full build.
-      autoWire = [ ];
     };
-    packages.default =
-      pkgs.haskell.lib.compose.overrideCabal
-        (drv: {
-          # NOTE: `inputs.prev != inputs.self` is a very loose check and expects the user
-          # to do the right thing when overriding `prev`
-          previousIntermediates = if inputs.prev != inputs.self then inputs'.prev.packages.default else null;
-        })
-        config.haskellProjects.default.outputs.finalPackages.haskell-incremental-build-template;
+
+    packages.default = self'.packages.haskell-incremental-build-template;
   };
 }
