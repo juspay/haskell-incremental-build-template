0c2e24abc2bccc56db9a7a8cb31c5ecc00de35f1
shivaraj-bh <me@shivaraj-bh.in>
2025-05-07 17:54:15 +0530

    ci: Add custom github action

--- .github/actions/nix-incremental/find_intermediates_base.sh
+++ .github/actions/nix-incremental/find_intermediates_base.sh
@@ -3,23 +3,21 @@
 # Preserve original stdout in fd 3, then redirect all stdout to stderr
 exec 3>&1 1>&2
 
+repo="$1"
+package_name="$2"
+cache_url="$3"
+
 for COMMIT in $(git log -n 5 --pretty=%H); do
   echo "Testing commit $COMMIT"
-  WORKTREE_DIR="temp-worktree-$COMMIT"
-  git worktree add $WORKTREE_DIR $COMMIT
-  cd $WORKTREE_DIR
-  # Check if the `default` package of this commit revision is present in the cache.
+  # Check if the `packages.<package_name>` flake output of this commit revision is present in the cache.
   #
   # Note: Only checks for the presence, doesn't pull the path from the cache.
-  if nix path-info $(nix eval .#default.outPath | tr -d '"') --store https://cache.nixos.asia/oss; then
+  if nix path-info $(nix eval $REPO/$COMMIT#$package_name.outPath | tr -d '"') --store $cache_url; then
     echo "Intermediates exist for $COMMIT"
     # Output only the commit hash to original stdout (fd 3)
     echo "$COMMIT" >&3
-    git worktree remove $WORKTREE_DIR
     exit 0
   else
     echo "Intermediates do not exist for $COMMIT"
-    git worktree remove $WORKTREE_DIR
   fi
-  cd -
 done
