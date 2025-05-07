d733282048dcf5c30c43ef9396d83881acfce5a1
shivaraj-bh <me@shivaraj-bh.in>
2025-05-07 18:35:20 +0530

    shell: fix naming

--- .github/actions/nix-incremental/find_intermediates_base.sh
+++ .github/actions/nix-incremental/find_intermediates_base.sh
@@ -7,17 +7,17 @@ repo="$1"
 package_name="$2"
 cache_url="$3"
 
-for COMMIT in $(git log -n 5 --pretty=%H); do
-  echo "Testing commit $COMMIT"
+for commit in $(git log -n 5 --pretty=%H); do
+  echo "Testing $commit"
   # Check if the `packages.<package_name>` flake output of this commit revision is present in the cache.
   #
   # Note: Only checks for the presence, doesn't pull the path from the cache.
-  if nix path-info $(nix eval $REPO/$COMMIT#$package_name.outPath | tr -d '"') --store $cache_url; then
-    echo "Intermediates exist for $COMMIT"
+  if nix path-info $(nix eval $repo/$commit#$package_name.outPath | tr -d '"') --store $cache_url; then
+    echo "Intermediates exist for $commit"
     # Output only the commit hash to original stdout (fd 3)
-    echo "$COMMIT" >&3
+    echo "$commit" >&3
     exit 0
   else
-    echo "Intermediates do not exist for $COMMIT"
+    echo "Intermediates do not exist for $commit"
   fi
 done
